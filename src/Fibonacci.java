13028d2ff105ac24ba0febb52d0b5a8c5d02f5a9
sebastian-Arellano <69532133+sebastian-Arellano@users.noreply.github.com>
2020-08-11 21:19:00 -0600

    Initial commit

---
+++ src/Fibonacci.java
@@ -0,0 +1,23 @@
+import java.util.Scanner;
+
+public class Fibonacci{
+  public static void main(String args[]){
+	  
+ Scanner x = new Scanner (System.in);
+ 
+   int i = 0, a = 0, b = 1, c = 0, A;
+   
+   System.out.println("ingrese numero de serie  a consultar:");
+   A=x.nextInt();
+   
+   for(i = 0; i < A; i++){
+    
+    
+     c = a + b;
+     a = b;
+     b = c;
+     
+    }
+   System.out.print(a + ",");
+  }
+}
